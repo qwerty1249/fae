c137e60e4b4da60c5792e927454399cce34be638
1000095984 <averagea1gamer@gmail.com>
2023-04-11 20:52:43 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dyonjjwxn/image/upload",
+  }, //url for image upload to cloudinary. First copy "API base url" from Cloudinary. Then paste it here.
+  
+};
